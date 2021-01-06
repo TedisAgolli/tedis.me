959a6a8b22ebf9543655defa47f373c72f0d2b41
TedisAgolli <tedisagolli@gmail.com>
2021-01-05 22:41:19 -0500

    Upgrade to tailwind 2.0

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-    plugins: [
-        require('postcss-import'),
-        require('tailwindcss'),
-        require('autoprefixer'),
-        require('postcss-preset-env')({ stage: 1 }),
-        require('postcss-clean'),
-    ],
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("postcss-preset-env")({ stage: 1 }),
+  ],
 };
